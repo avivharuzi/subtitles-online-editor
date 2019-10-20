a9acc59ae2cf34b7a7577e6ba5cf5bd7e6c911e0
Aviv Haruzi <avivharuzi@gmail.com>
2019-10-20 13:21:29 +0300

    Add logic to home page and not found page

--- src/app/core/components/home/home.component.ts
+++ src/app/core/components/home/home.component.ts
@@ -1,8 +1,78 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
+
+import { SUBTITLE_ENCODINGS } from '../../../subtitles/shared/subtitle-encodings';
+import { SubtitleConverter } from '../../../subtitles/shared/subtitle-converter';
+import { SubtitleEncoding } from '../../../subtitles/shared/subtitle-encoding.interface';
+import { SubtitleService } from '../../../subtitles/shared/subtitle.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  subtitleEncodings: SubtitleEncoding[];
+  subtitleEncodingSelected: string;
+  subtitleEncodingExportSelected: string;
+  file: File;
+  errors: string[];
+
+  constructor(
+    public subtitleService: SubtitleService,
+    private renderer: Renderer2,
+  ) {
+    this.subtitleEncodings = SUBTITLE_ENCODINGS;
+    this.subtitleEncodingSelected = 'UTF-8';
+    this.subtitleEncodingExportSelected = 'UTF-8';
+  }
+
+  onChangedFiles({ files, errors }: { files: FileList, errors: string[] }): void {
+    this.errors = errors;
+
+    if (errors.length === 0 && files.length > 0) {
+      this.file = files.item(0);
+    } else {
+      this.file = null;
+    }
+  }
+
+  startEdit(): void {
+    if (!this.file) {
+      return;
+    }
+
+    SubtitleConverter.getTextFromFile(this.file, this.subtitleEncodingSelected).subscribe(text => {
+      try {
+        const subtitles = SubtitleConverter.getSubtitlesFromText(text);
+        this.subtitleService.setSubtitles(subtitles);
+        this.subtitleService.setSubtitlesFilename(this.file.name);
+        this.subtitleEncodingExportSelected = this.subtitleEncodingSelected;
+        this.file = null;
+        this.errors = null;
+      } catch (e) {
+        this.errors = [e];
+      }
+    });
+  }
+
+  download(filename: string): void {
+    if (!this.subtitleService.isInEditMode()) {
+      return;
+    }
+
+    if (!filename) {
+      return;
+    }
+
+    const text = SubtitleConverter.getTextFromSubtitles(this.subtitleService.getSubtitles());
+
+    const element = this.renderer.createElement('a');
+    this.renderer.setAttribute(element, 'href', `data:text/plain;charset=${this.subtitleEncodingExportSelected},`
+      + encodeURIComponent(text));
+    this.renderer.setAttribute(element, 'download', `${filename}.srt`);
+    this.renderer.setStyle(element, 'display', 'none');
+    this.renderer.appendChild(document.body, element);
+    element.click();
+    this.renderer.removeChild(document.body, element);
+  }
+}
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,11 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { AlertModule } from 'ngx-bootstrap/alert';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { NgxMaskModule } from 'ngx-mask';
 
+import { SharedModule } from '../shared/shared.module';
 import { HomeComponent } from './components/home/home.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
   declarations: [HomeComponent, NotFoundComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule,
+    AlertModule.forRoot(),
+    BsDropdownModule.forRoot(),
+    TabsModule.forRoot(),
+    NgxMaskModule.forRoot(),
+    SharedModule,
+  ],
 })
 export class CoreModule {}
