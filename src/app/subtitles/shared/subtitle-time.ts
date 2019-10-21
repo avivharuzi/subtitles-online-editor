4ffbd07f024d2ed53eca683a7c494424b19df189
Aviv Haruzi <avivharuzi@gmail.com>
2019-10-21 13:24:37 +0300

    Fix time algorithm problem

--- src/app/subtitles/shared/subtitle-time.ts
+++ src/app/subtitles/shared/subtitle-time.ts
@@ -2,18 +2,45 @@ export class SubtitleTime {
   private static readonly charsToRemove = /:|,/g;
 
   static getTimePlus(time: string, n: number): string {
-    return this.getTime(+time.replace(this.charsToRemove, '') + n);
+    return this.isTimeValid(time) ? this.getTime(this.getMs(time) + n) : time;
   }
 
   static getTimeMinus(time: string, n: number): string {
-    return this.getTime(+time.replace(this.charsToRemove, '') - n);
+    return this.isTimeValid(time) ? this.getTime(this.getMs(time) - n) : time;
+  }
+
+  private static isTimeValid(time: string) {
+    return time.length === 12;
+  }
+
+  private static getMs(time: string): number {
+    const times = time.split(this.charsToRemove);
+    const hh = +times[0];
+    const mm = +times[1];
+    const ss = +times[2];
+    const ms = +times[3];
+
+    return ms + (ss * 1000) + (mm * 60 * 1000) + (hh * 60 * 60 * 1000);
   }
 
   private static getTime(n: number): string {
-    let s = n.toString();
-    if (s.length !== 9) {
-      s = '0'.repeat(9 - s.length) + s;
+    if (n < 0) {
+      n = 0;
     }
-    return `${s[0]}${s[1]}:${s[2]}${s[3]}:${s[4]}${s[5]},${s[6]}${s[7]}${s[8]}`;
+
+    const ms = n % 1000;
+    n = (n - ms) / 1000;
+    const seconds = n % 60;
+    n = (n - seconds) / 60;
+    const minutes = n % 60;
+    const hours = (n - minutes) / 60;
+
+    return this.pad(hours) + ':' + this.pad(minutes) + ':' + this.pad(seconds) + ',' + this.pad(ms, 3);
+  }
+
+  private static pad(n: number, z?: number) {
+    z = z || 2;
+
+    return ('00' + n).slice(-z);
   }
 }
